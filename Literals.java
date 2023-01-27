715c512028ac6879c17809fec7c24f4bb9960bd9
HarryPo30 <123658884+HarryPo30@users.noreply.github.com>
2023-01-27 16:12:32 +0530

    Add files via upload

---
+++ Literals.java
@@ -0,0 +1,20 @@
+public class Literals {
+    public static void main(String[] args){
+
+        byte id=89;
+        int age=56;
+        float num=12.5f;
+        double num2=14.5566;
+        char name='H';
+        boolean a= true;
+        long num3=234566543456L;
+
+        System.out.println(id);
+        System.out.println(age);
+        System.out.println(num);
+        System.out.println(num2);
+        System.out.println(name);
+        System.out.println(a);
+        System.out.println(num3);
+    }
+}
